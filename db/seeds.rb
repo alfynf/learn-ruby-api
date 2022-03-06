677c19dfd38fbd4377b8b6d8757b949787e5aa9d
alfynf <alfynurf36@gmail.com>
2022-03-07 00:07:01 +0700

    create api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+menu1 = SecretMenuItem.create(menu_name: "Nasi Goreng", restaurant_name: "Warung Ma Icih", menu_description: "Nasi goreng dan kerupuk")
+menu2 = SecretMenuItem.create(menu_name: "Spagetti", restaurant_name: "Restoran Mahal", menu_description: "Spagetti dengan daging dan saus tomat")
+menu3 = SecretMenuItem.create(menu_name: "Teh Anget", restaurant_name: "Warteg Kiri", menu_description: "Teh Anget Manis dengan Lemon")
